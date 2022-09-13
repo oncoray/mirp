fcffabe97a1376748dd267afbe2f5be11a1d5f42
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2022-09-13 13:31:20 +0200

    Added unit test skeletons for IBSI 2 phase 2.
    These are currently generative, instead of being tests.

--- test/ibsi_2_feature_test.py
+++ test/ibsi_2_feature_test.py
@@ -8,16 +8,18 @@ from mirp.importSettings import SettingsClass, GeneralSettingsClass, ImagePostPr
 # Find path to the test directory. This is because we need to read datafiles stored in subdirectories.
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 
-PERTURB_IMAGES = False
+PERTURB_IMAGES = True
 WRITE_TEMP_FILES = True
 
 
-def _get_default_settings(by_slice: bool = False,
+def _get_default_settings(by_slice: bool,
+                          configuration_id: str,
                           base_feature_families="none"):
     """Set default settings for response map tests."""
 
     general_settings = GeneralSettingsClass(
-        by_slice=by_slice
+        by_slice=by_slice,
+        config_str=configuration_id
     )
 
     # 2D-analysis does not use interpolation, whereas the volumetric (3D) analysis does.
@@ -43,10 +45,14 @@ def _get_default_settings(by_slice: bool = False,
     )
 
     if PERTURB_IMAGES:
-        # TODO: add in perturbation settings from IBSI 1.
         perturbation_settings = ImagePerturbationSettingsClass(
-            crop_around_roi=False
+            crop_around_roi=False,
+            perturbation_rotation_angles=[-15.0, -10.0, 5.0, 0.0, 5.0, 10.0, 15.0],
+            perturbation_translation_fraction=[0.00, 0.25, 0.50, 0.75],
+            perturbation_roi_adapt_size=[-2.0, 0.0, 2.0],
+            perturbation_roi_adapt_type="distance"
         )
+
     else:
         perturbation_settings = ImagePerturbationSettingsClass(
             crop_around_roi=False
@@ -74,6 +80,7 @@ def _process_experiment(configuration_id: str,
     # Get default settings.
     general_settings, image_interpolation_settings, feature_computation_parameters, resegmentation_settings, \
         perturbation_settings = _get_default_settings(by_slice=by_slice,
+                                                      configuration_id=configuration_id,
                                                       base_feature_families=base_feature_families)
 
     settings = SettingsClass(
@@ -127,7 +134,7 @@ def test_ibsi_2_config_none():
     Compare computed feature values with reference values for configurations 1A and 1B of IBSI 2 phase 2.
     """
 
-    # Configure image transformation settings.
+    # Configuration 1.A ------------------------------------------------------------------------------------------------
     image_transformation_settings = ImageTransformationSettingsClass(
         by_slice=True,
         response_map_feature_families="statistics",
@@ -137,7 +144,7 @@ def test_ibsi_2_config_none():
     )
 
     data = _process_experiment(
-        configuration_id="1A",
+        configuration_id="1.A",
         by_slice=True,
         image_transformation_settings=image_transformation_settings,
         base_feature_families="statistics"
@@ -145,6 +152,7 @@ def test_ibsi_2_config_none():
 
     # TODO assert stuff one IBSI 2 is done.
 
+    # Configuration 1.B ------------------------------------------------------------------------------------------------
     image_transformation_settings = ImageTransformationSettingsClass(
         by_slice=False,
         response_map_feature_families="statistics",
@@ -154,7 +162,7 @@ def test_ibsi_2_config_none():
     )
 
     data = _process_experiment(
-        configuration_id="1B",
+        configuration_id="1.B",
         by_slice=False,
         image_transformation_settings=image_transformation_settings,
         base_feature_families="statistics"
@@ -163,12 +171,12 @@ def test_ibsi_2_config_none():
     # TODO assert stuff one IBSI 2 is done.
 
 
-def test_ibsi_2_config_mean():
+def test_ibsi_2_config_mean_filter():
     """
-    Compare computed feature values with reference values for configurations 1A and 1B of IBSI 2 phase 2.
+    Compare computed feature values with reference values for configurations 2A and 2B of IBSI 2 phase 2.
     """
 
-    # Configure image transformation settings.
+    # Configuration 2.A ------------------------------------------------------------------------------------------------
     image_transformation_settings = ImageTransformationSettingsClass(
         by_slice=True,
         response_map_feature_families="statistics",
@@ -179,13 +187,14 @@ def test_ibsi_2_config_mean():
     )
 
     data = _process_experiment(
-        configuration_id="2A",
+        configuration_id="2.A",
         by_slice=True,
         image_transformation_settings=image_transformation_settings
     )
 
     # TODO assert stuff one IBSI 2 is done.
 
+    # Configuration 2.B ------------------------------------------------------------------------------------------------
     image_transformation_settings = ImageTransformationSettingsClass(
         by_slice=False,
         response_map_feature_families="statistics",
@@ -196,7 +205,333 @@ def test_ibsi_2_config_mean():
     )
 
     data = _process_experiment(
-        configuration_id="2B",
+        configuration_id="2.B",
+        by_slice=False,
+        image_transformation_settings=image_transformation_settings
+    )
+
+    # TODO assert stuff one IBSI 2 is done.
+
+
+def test_ibsi_2_config_laplacian_of_gaussian_filter():
+    """
+    Compare computed feature values with reference values for configurations 3A and 3B of IBSI 2 phase 2.
+    """
+
+    # Configuration 3.A ------------------------------------------------------------------------------------------------
+    image_transformation_settings = ImageTransformationSettingsClass(
+        by_slice=True,
+        response_map_feature_families="statistics",
+        response_map_feature_settings=None,
+        boundary_condition="reflect",
+        filter_kernels="laplacian_of_gaussian",
+        laplacian_of_gaussian_sigma=1.5,
+        laplacian_of_gaussian_kernel_truncate=4.0
+    )
+
+    data = _process_experiment(
+        configuration_id="3.A",
+        by_slice=True,
+        image_transformation_settings=image_transformation_settings
+    )
+
+    # TODO assert stuff one IBSI 2 is done.
+
+    # Configuration 3.B ------------------------------------------------------------------------------------------------
+    image_transformation_settings = ImageTransformationSettingsClass(
+        by_slice=False,
+        response_map_feature_families="statistics",
+        response_map_feature_settings=None,
+        boundary_condition="reflect",
+        filter_kernels="laplacian_of_gaussian",
+        laplacian_of_gaussian_sigma=1.5,
+        laplacian_of_gaussian_kernel_truncate=4.0
+    )
+
+    data = _process_experiment(
+        configuration_id="3.B",
+        by_slice=False,
+        image_transformation_settings=image_transformation_settings
+    )
+
+    # TODO assert stuff one IBSI 2 is done.
+
+
+def test_ibsi_2_config_laws_filter():
+    """
+    Compare computed feature values with reference values for configurations 4A and 4B of IBSI 2 phase 2.
+    """
+
+    # Configuration 4.A ------------------------------------------------------------------------------------------------
+    image_transformation_settings = ImageTransformationSettingsClass(
+        by_slice=True,
+        response_map_feature_families="statistics",
+        response_map_feature_settings=None,
+        boundary_condition="reflect",
+        filter_kernels="laws",
+        laws_kernel="L5E5",
+        laws_compute_energy=True,
+        laws_rotation_invariance=True,
+        laws_delta=7,
+        laws_pooling_method="max"
+    )
+
+    data = _process_experiment(
+        configuration_id="4.A",
+        by_slice=True,
+        image_transformation_settings=image_transformation_settings
+    )
+
+    # TODO assert stuff one IBSI 2 is done.
+
+    # Configuration 4.B ------------------------------------------------------------------------------------------------
+    image_transformation_settings = ImageTransformationSettingsClass(
+        by_slice=False,
+        response_map_feature_families="statistics",
+        response_map_feature_settings=None,
+        boundary_condition="reflect",
+        filter_kernels="laws",
+        laws_kernel="L5E5E5",
+        laws_compute_energy=True,
+        laws_rotation_invariance=True,
+        laws_delta=7,
+        laws_pooling_method="max"
+    )
+
+    data = _process_experiment(
+        configuration_id="4.B",
+        by_slice=False,
+        image_transformation_settings=image_transformation_settings
+    )
+
+    # TODO assert stuff one IBSI 2 is done.
+
+
+def test_ibsi_2_config_gabor_filter():
+    """
+    Compare computed feature values with reference values for configurations 5A and 5B of IBSI 2 phase 2.
+    """
+
+    # Configuration 5.A ------------------------------------------------------------------------------------------------
+    image_transformation_settings = ImageTransformationSettingsClass(
+        by_slice=True,
+        response_map_feature_families="statistics",
+        response_map_feature_settings=None,
+        boundary_condition="reflect",
+        filter_kernels="gabor",
+        gabor_sigma=5.0,
+        gabor_gamma=1.5,
+        gabor_lambda=2.0,
+        gabor_theta=0.0,
+        gabor_theta_step=22.5,
+        gabor_response="modulus",
+        gabor_rotation_invariance=True,
+        gabor_pooling_method="mean"
+    )
+
+    data = _process_experiment(
+        configuration_id="5.A",
+        by_slice=True,
+        image_transformation_settings=image_transformation_settings
+    )
+
+    # TODO assert stuff one IBSI 2 is done.
+
+    # Configuration 5.B ------------------------------------------------------------------------------------------------
+    image_transformation_settings = ImageTransformationSettingsClass(
+        by_slice=False,
+        response_map_feature_families="statistics",
+        response_map_feature_settings=None,
+        boundary_condition="reflect",
+        filter_kernels="gabor",
+        gabor_sigma=5.0,
+        gabor_gamma=1.5,
+        gabor_lambda=2.0,
+        gabor_theta=0.0,
+        gabor_theta_step=22.5,
+        gabor_response="modulus",
+        gabor_rotation_invariance=True,
+        gabor_pooling_method="mean"
+    )
+
+    data = _process_experiment(
+        configuration_id="5.B",
+        by_slice=False,
+        image_transformation_settings=image_transformation_settings
+    )
+
+    # TODO assert stuff one IBSI 2 is done.
+
+
+def test_ibsi_2_config_daubechies_filter():
+    """
+    Compare computed feature values with reference values for configurations 6A, 6B, 7A and 7B of IBSI 2 phase 2.
+    """
+
+    # Configuration 6.A ------------------------------------------------------------------------------------------------
+    image_transformation_settings = ImageTransformationSettingsClass(
+        by_slice=True,
+        response_map_feature_families="statistics",
+        response_map_feature_settings=None,
+        boundary_condition="reflect",
+        filter_kernels="separable_wavelet",
+        separable_wavelet_families="db3",
+        separable_wavelet_set="LH",
+        separable_wavelet_rotation_invariance=True,
+        separable_wavelet_decomposition_level=1,
+        separable_wavelet_pooling_method="mean"
+    )
+
+    data = _process_experiment(
+        configuration_id="6.A",
+        by_slice=True,
+        image_transformation_settings=image_transformation_settings
+    )
+
+    # TODO assert stuff one IBSI 2 is done.
+
+    # Configuration 6.B ------------------------------------------------------------------------------------------------
+    image_transformation_settings = ImageTransformationSettingsClass(
+        by_slice=False,
+        response_map_feature_families="statistics",
+        response_map_feature_settings=None,
+        boundary_condition="reflect",
+        filter_kernels="separable_wavelet",
+        separable_wavelet_families="db3",
+        separable_wavelet_set="LLH",
+        separable_wavelet_rotation_invariance=True,
+        separable_wavelet_decomposition_level=1,
+        separable_wavelet_pooling_method="mean"
+    )
+
+    data = _process_experiment(
+        configuration_id="6.B",
+        by_slice=False,
+        image_transformation_settings=image_transformation_settings
+    )
+
+    # TODO assert stuff one IBSI 2 is done.
+
+    # Configuration 7.A ------------------------------------------------------------------------------------------------
+    image_transformation_settings = ImageTransformationSettingsClass(
+        by_slice=True,
+        response_map_feature_families="statistics",
+        response_map_feature_settings=None,
+        boundary_condition="reflect",
+        filter_kernels="separable_wavelet",
+        separable_wavelet_families="db3",
+        separable_wavelet_set="HH",
+        separable_wavelet_rotation_invariance=True,
+        separable_wavelet_decomposition_level=2,
+        separable_wavelet_pooling_method="mean"
+    )
+
+    data = _process_experiment(
+        configuration_id="7.A",
+        by_slice=True,
+        image_transformation_settings=image_transformation_settings
+    )
+
+    # TODO assert stuff one IBSI 2 is done.
+
+    # Configuration 7.B ------------------------------------------------------------------------------------------------
+    image_transformation_settings = ImageTransformationSettingsClass(
+        by_slice=False,
+        response_map_feature_families="statistics",
+        response_map_feature_settings=None,
+        boundary_condition="reflect",
+        filter_kernels="separable_wavelet",
+        separable_wavelet_families="db3",
+        separable_wavelet_set="HHH",
+        separable_wavelet_rotation_invariance=True,
+        separable_wavelet_decomposition_level=2,
+        separable_wavelet_pooling_method="mean"
+    )
+
+    data = _process_experiment(
+        configuration_id="7.B",
+        by_slice=False,
+        image_transformation_settings=image_transformation_settings
+    )
+
+    # TODO assert stuff one IBSI 2 is done.
+
+
+def test_ibsi_2_config_simoncelli_filter():
+    """
+    Compare computed feature values with reference values for configurations 8A, 8B, 9A and 9B of IBSI 2 phase 2.
+    """
+
+    # Configuration 8.A ------------------------------------------------------------------------------------------------
+    image_transformation_settings = ImageTransformationSettingsClass(
+        by_slice=True,
+        response_map_feature_families="statistics",
+        response_map_feature_settings=None,
+        boundary_condition="reflect",
+        filter_kernels="nonseparable_wavelet",
+        nonseparable_wavelet_families="simoncelli",
+        nonseparable_wavelet_decomposition_level=1,
+    )
+
+    data = _process_experiment(
+        configuration_id="8.A",
+        by_slice=True,
+        image_transformation_settings=image_transformation_settings
+    )
+
+    # TODO assert stuff one IBSI 2 is done.
+
+    # Configuration 8.B ------------------------------------------------------------------------------------------------
+    image_transformation_settings = ImageTransformationSettingsClass(
+        by_slice=False,
+        response_map_feature_families="statistics",
+        response_map_feature_settings=None,
+        boundary_condition="reflect",
+        filter_kernels="nonseparable_wavelet",
+        nonseparable_wavelet_families="simoncelli",
+        nonseparable_wavelet_decomposition_level=1,
+    )
+
+    data = _process_experiment(
+        configuration_id="8.B",
+        by_slice=False,
+        image_transformation_settings=image_transformation_settings
+    )
+
+    # TODO assert stuff one IBSI 2 is done.
+
+    # Configuration 9.A ------------------------------------------------------------------------------------------------
+    image_transformation_settings = ImageTransformationSettingsClass(
+        by_slice=True,
+        response_map_feature_families="statistics",
+        response_map_feature_settings=None,
+        boundary_condition="reflect",
+        filter_kernels="nonseparable_wavelet",
+        nonseparable_wavelet_families="simoncelli",
+        nonseparable_wavelet_decomposition_level=2,
+    )
+
+    data = _process_experiment(
+        configuration_id="9.A",
+        by_slice=True,
+        image_transformation_settings=image_transformation_settings
+    )
+
+    # TODO assert stuff one IBSI 2 is done.
+
+    # Configuration 9.B ------------------------------------------------------------------------------------------------
+    image_transformation_settings = ImageTransformationSettingsClass(
+        by_slice=False,
+        response_map_feature_families="statistics",
+        response_map_feature_settings=None,
+        boundary_condition="reflect",
+        filter_kernels="nonseparable_wavelet",
+        nonseparable_wavelet_families="simoncelli",
+        nonseparable_wavelet_decomposition_level=2,
+    )
+
+    data = _process_experiment(
+        configuration_id="9.B",
         by_slice=False,
         image_transformation_settings=image_transformation_settings
     )
