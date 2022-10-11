7e8a8a46a806576bd26f37278c7402755c9fca74
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2022-10-11 21:04:18 +0200

    Deprecated contour_to_image_space

--- mirp/contourClass.py
+++ mirp/contourClass.py
@@ -15,17 +15,12 @@ class ContourClass:
         self.contour=vertices
         self.sop_instance_uid = sop_instance_uid
 
-    def contour_to_image_space(self, img_obj: ImageClass):
-
-        contour_vox = np.dot(img_obj.m_affine_inv, np.transpose(self.contour - img_obj.origin))
-
-        return np.transpose(contour_vox)
-
     def contour_to_grid_ray_cast(self, img_obj):
         from mirp.morphologyUtilities import poly2grid
 
         # Convert contours to voxel space
-        contour_vox = self.contour_to_image_space(img_obj=img_obj)
+        contour_vox = img_obj.to_voxel_coordinates(np.transpose(self.contour))
+        contour_vox = np.transpose(contour_vox)
 
         # Reduce numerical issues by rounding precision
         contour_vox[:, 0] = np.rint(contour_vox[:, 0])
