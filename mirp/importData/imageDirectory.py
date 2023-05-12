d51b06bfa40e17996af310dc4e134262aa70a825
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2023-05-12 17:24:17 +0200

    Different approach for finding directories, based on elimination.

--- mirp/importData/imageDirectory.py
+++ mirp/importData/imageDirectory.py
@@ -1,10 +1,11 @@
 import os
 import os.path
 import copy
+import warnings
 
 from typing import Union, List
 from itertools import chain
-from mirp.importData.utilities import supported_file_types
+from mirp.importData.utilities import supported_file_types, dir_structure_contains_directory
 from mirp.importData.imageGenericFile import ImageFile
 
 
@@ -21,8 +22,11 @@ class ImageDirectory:
             contain_multiple_samples: [bool] = False,
             **kwargs):
 
+        if sample_name is not None and not isinstance(sample_name, list):
+            sample_name = [sample_name]
+
         self.image_directory = directory
-        self.sample_name = sample_name
+        self.sample_name: Union[None, List[str]] = sample_name
         self.image_name = image_name
         self.sub_folder = image_sub_folder
         self.modality = image_modality
@@ -43,7 +47,6 @@ class ImageDirectory:
         return False
 
     def _generate_sub_directories(self):
-        # Generates subdirectories
 
         # Copy sample names. These are candidates for the names of subdirectories.
         sample_name = copy.deepcopy(self.sample_name)
@@ -137,17 +140,68 @@ class ImageDirectory:
 
         if not isinstance(self.image_directory, str):
             if raise_error:
-                raise TypeError("A directory containing images was expected. Found: an object that was not a path to a "
-                                "directory.")
+                raise TypeError(
+                    f"A directory containing images was expected. Found: an object that was not a path to a "
+                    f"directory.")
 
             return False
 
         if not os.path.isdir(self.image_directory):
             if raise_error:
-                raise TypeError("A directory containing images was expected. Found: a path that was not a path to an "
-                                "existing directory.")
+                raise TypeError(
+                    f"A directory containing images was expected. Found: a path that was not a path to an existing "
+                    f"directory: {self.image_directory}")
 
             return False
+
+        # Find potentially valid directory paths.
+        path_info = list(os.walk(self.image_directory))
+
+        if len(path_info) == 0:
+            ValueError(f"The {self.image_directory} directory is empty, and no images could be found.")
+
+        # Find entries where the folder structure matches the sub_folder.
+        if self.sub_folder is not None:
+            path_info = [
+                path_info_element for path_info_element in path_info
+                if path_info_element[0].endswith(self.sub_folder)
+            ]
+
+            if len(path_info) == 0:
+                raise ValueError(
+                    f"No directories where found in {self.image_directory} that contained the directory "
+                    f"substructure {self.sub_folder}.")
+
+        # Find entries where the folder structure contains a sample name. All the sample names must be present to
+        # avoid incidental findings.
+        if self.sample_name is not None:
+            all_samples_selected = True
+            sample_name_matches = []
+            ignore_dirs = [self.image_directory]
+            if self.sub_folder is not None:
+                ignore_dirs += [self.sub_folder]
+
+            for sample_name in self.sample_name:
+                current_sample_matches = [
+                    ii for ii in range(len(path_info))
+                    if dir_structure_contains_directory(path_info[ii][0], pattern=sample_name, ignore_dir=ignore_dirs)
+                ]
+
+                if len(current_sample_matches) > 0:
+                    sample_name_matches += current_sample_matches
+
+                else:
+                    all_samples_selected = False
+                    break
+
+            # Only filter list if all sample names are uniquely part of their respective paths.
+            if all_samples_selected and len(set(sample_name_matches)) == len(sample_name_matches) and len(
+                    sample_name_matches) > 0:
+                path_info = [path_info[ii] for ii in sample_name_matches]
+
+        # Find entries that include files of the right file-type.
+        if self.file_type is not None:
+            allowed_file_extensions = supported_file_types(file_type=self.file_type)
 
         # Generate paths to directories.
         valid_directories = [self._generate_sub_directories()]
