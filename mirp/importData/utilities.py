cc33c36cedf142b789fa14feb219da0c322aa910
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2023-05-12 16:33:19 +0200

    Helper functions for path matching.

--- mirp/importData/utilities.py
+++ mirp/importData/utilities.py
@@ -1,4 +1,5 @@
-from typing import Union
+from typing import Union, List
+from os.path import split
 
 
 def supported_image_modalities(modality: Union[None, str] = None):
@@ -71,3 +72,78 @@ def flatten_list(unflattened_list):
 
 def create_sample_name():
     ...
+
+
+def path_to_parts(x: str) -> List[str]:
+    """
+    Split a path into its components.
+    :param x: a string or path.
+    :return: a list of path components.
+    """
+
+    path_parts = []
+    while True:
+        x_head, x_tail = split(x)
+        path_parts += [x_tail]
+        if x_head == "":
+            break
+
+    return list(reversed(path_parts))
+
+
+def dir_structure_contains_directory(
+        x: str,
+        pattern: Union[str, List[str]],
+        ignore_dir: Union[None, str, List[str]]
+) -> bool:
+    """
+    Identify if a path contains a directory matches any of pattern.
+    :param x: a string or a path.
+    :param pattern: a pattern that should be fully matched in the path.
+    :param ignore_dir: any (partial) path that should be ignored. These are stripped from x prior to pattern matching.
+    :return: a boolean value.
+    """
+    # Split x into parts.
+    x = path_to_parts(x)
+
+    # Strip the pattern to be ignored from x, if possible.
+    if ignore_dir is not None:
+        if not isinstance(ignore_dir, list):
+            ignore_dir = [ignore_dir]
+
+        for current_ignore_dir in ignore_dir:
+            current_ignore_dir = path_to_parts(current_ignore_dir)
+
+            # Find matching sequential elements.
+            match_index: List[Union[None, int]] = [None for ii in range(len(current_ignore_dir))]
+            for jj, ignore_elem in enumerate(current_ignore_dir):
+                if match_index[0] is None:
+                    if jj > 0:
+                        # Break if the first element is not found at all.
+                        break
+                    for ii in range(len(x)):
+                        if x[ii] == ignore_elem:
+                            match_index[0] = ii
+                            break
+                else:
+                    if match_index[jj - 1] + 1 > len(x) - 1:
+                        # Break if we would exceed the length of x.
+                        break
+
+                    if x[match_index[jj - 1] + 1] == ignore_elem:
+                        match_index[jj] = match_index[jj - 1] + 1
+                    else:
+                        # Each element must be sequential.
+                        break
+
+            if not any(match_elem is None for match_elem in match_index):
+                x = [x_elem for ii, x_elem in enumerate(x) if ii not in match_index]
+
+    if len(x) == 0 or x == "":
+        return False
+
+    if not isinstance(pattern, list):
+        pattern = [pattern]
+
+    # Find if any pattern is exactly matched.
+    return any(x_elem in pattern for x_elem in x)
