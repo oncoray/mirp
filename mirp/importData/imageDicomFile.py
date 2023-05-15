1f8208c38bc87cc36c80269229ecde0db7dc3724
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2023-05-15 15:46:29 +0200

    Updated method to set sample name.

--- mirp/importData/imageDicomFile.py
+++ mirp/importData/imageDicomFile.py
@@ -93,17 +93,17 @@ class ImageDicomFile(ImageFile):
     def complete(self):
 
         # Read DICOM file.
-        dcm = dcmread(self.file_path,
-                      stop_before_pixels=True,
-                      force=True)
+        dcm = dcmread(
+            self.file_path,
+            stop_before_pixels=True,
+            force=True)
 
         # Attempt to set the modality attribute.
         if self.modality is None:
             self.modality = get_pydicom_meta_tag(dcm_seq=dcm, tag=(0x0008, 0x0060), tag_type="str")
 
         if self.modality is None:
-            warn(f"Could not ascertain the modality from the DICOM file ({self.file_path}).",
-                 UserWarning)
+            warn(f"Could not ascertain the modality from the DICOM file ({self.file_path}).", UserWarning)
 
             self.modality = "generic"
         else:
@@ -115,13 +115,33 @@ class ImageDicomFile(ImageFile):
         # Set Frame of Reference UID (if any)
         self.frame_of_reference_uid = get_pydicom_meta_tag(dcm_seq=dcm, tag=(0x0020, 0x0052), tag_type="str")
 
-        # Set patient name if its not known. First try the patient ID attribute, and subsequently patient name.
-        if self.sample_name is None:
-            self.sample_name = get_pydicom_meta_tag(dcm_seq=dcm, tag=(0x0010, 0x0020), tag_type="str")
 
-        if self.sample_name is None:
-            temporary_name = get_pydicom_meta_tag(dcm_seq=dcm, tag=(0x0010, 0x0010), tag_type="str")
+        # Set sample name -- note that if sample_name is a (single) string, it is neither replaced nor updated.
+        if self.sample_name is None or isinstance(self.sample_name, list):
+            # Consider the following DICOM elements:
+            # patient id, study id, patient name, series description and study description.
+            # These are explicitly ordered by relevance for setting the sample name.
+            dicom_sample_name = [
+                get_pydicom_meta_tag(dcm_seq=dcm, tag=(0x0010, 0x0020), tag_type="str"),
+                get_pydicom_meta_tag(dcm_seq=dcm, tag=(0x0020, 0x0010), tag_type="str"),
+                get_pydicom_meta_tag(dcm_seq=dcm, tag=(0x0010, 0x0010), tag_type="str"),
+                get_pydicom_meta_tag(dcm_seq=dcm, tag=(0x0008, 0x1030), tag_type="str"),
+                get_pydicom_meta_tag(dcm_seq=dcm, tag=(0x0008, 0x103E), tag_type="str")
+            ]
 
-            if temporary_name is not None:
-                # Strip ^ from temporary name.
-                self.sample_name = temporary_name.strip("^_ ")
+            dicom_sample_name = [
+                current_dicom_sample_name for current_dicom_sample_name in dicom_sample_name
+                if current_dicom_sample_name is not None
+            ]
+
+            if self.sample_name is None and len(dicom_sample_name) > 0:
+                # Set sample name using the most relevant DICOM tag, if no sample names are present.
+                self.sample_name = dicom_sample_name[0]
+
+            elif len(self.sample_name) > 0 and len(dicom_sample_name) > 0:
+                # Set sample name using the intersection of allowed sample names and DICOM-based names.
+                matching_sample_names = set(self.sample_name).intersection(set(dicom_sample_name))
+                if len(matching_sample_names) > 0:
+                    self.sample_name = list(matching_sample_names)[0]
+                else:
+                    self.sample_name = None
