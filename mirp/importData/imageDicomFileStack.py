fc9283dcccf944b2c6a5085adce6889f7ed9306f
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2023-05-26 14:50:07 +0200

    DICOM stack attributes are now completed conditional on them not being present.

--- mirp/importData/imageDicomFileStack.py
+++ mirp/importData/imageDicomFileStack.py
@@ -18,6 +18,14 @@ class ImageDicomFileStack(ImageFileStack):
         super().__init__(image_file_objects, **kwargs)
 
     def complete(self, remove_metadata=True, force=False):
+        """
+        Fills out missing attributes in an image stack. Image parameters in DICOM stacks, by design,
+        are fully determined by the origin of all slices in the stack. This method then sorts the image file objects
+        by origin, and uses their relative positions to determine slice spacing and the orientation vector.
+        :param remove_metadata: Whether metadata (DICOM headers) should be removed after completing information.
+        :param force: Whether attributes are forced to update or not.
+        :return: nothing, attributes are updated in place.
+        """
 
         # Load metadata of every slice.
         self.load_metadata()
@@ -55,13 +63,14 @@ class ImageDicomFileStack(ImageFileStack):
             for ii in range(len(position_table))
         ]
 
-        # Set image origin.
-        self.image_origin = tuple(get_pydicom_meta_tag(
-            dcm_seq=self.image_file_objects[0].image_metadata,
-            tag=(0x0020, 0x0032),
-            tag_type="mult_float",
-            default=np.array([0.0, 0.0, 0.0])
-        )[::-1])
+        if self.image_origin is None:
+            # Set image origin.
+            self.image_origin = tuple(get_pydicom_meta_tag(
+                dcm_seq=self.image_file_objects[0].image_metadata,
+                tag=(0x0020, 0x0032),
+                tag_type="mult_float",
+                default=np.array([0.0, 0.0, 0.0])
+            )[::-1])
 
         # Set image spacing
         # Compute the distance between the origins of the slices. This is the slice spacing.
@@ -107,7 +116,8 @@ class ImageDicomFileStack(ImageFileStack):
             tag_type="mult_float")
 
         # Set image spacing.
-        self.image_spacing = tuple([image_slice_spacing, image_pixel_spacing[1], image_pixel_spacing[0]])
+        if self.image_spacing is None:
+            self.image_spacing = tuple([image_slice_spacing, image_pixel_spacing[1], image_pixel_spacing[0]])
 
         # Read orientation (Xx, Xy, Xz, Yx, Yy, Yz) from metadata.
         image_orientation = get_pydicom_meta_tag(
@@ -125,7 +135,8 @@ class ImageDicomFileStack(ImageFileStack):
         image_orientation += list(z_orientation)
 
         # Revert to z, y, x order and reshape to matrix.
-        self.image_orientation = np.reshape(image_orientation[::-1], [3, 3])
+        if self.image_orientation is None:
+            self.image_orientation = np.reshape(image_orientation[::-1], [3, 3])
 
         # Set image dimensions. First, find the number of rows (y) and columns (x) in the data set.
         n_x = get_pydicom_meta_tag(
@@ -137,7 +148,8 @@ class ImageDicomFileStack(ImageFileStack):
             tag=(0x0028, 0x010),
             tag_type="int")
 
-        self.image_dimension = tuple([len(position_table), n_y, n_x])
+        if self.image_dimension is None:
+            self.image_dimension = tuple([len(position_table), n_y, n_x])
 
         # Check if the complete data passes verification.
         self.check(raise_error=True, remove_metadata=False)
