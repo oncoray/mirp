a018f80fb70100fd16f1dcf7b4b0038a7d68d4c9
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2023-05-26 14:55:21 +0200

    Added complete method for image file stacks.

--- mirp/importData/imageNumpyFileStack.py
+++ mirp/importData/imageNumpyFileStack.py
@@ -1,3 +1,5 @@
+import copy
+
 from typing import List
 
 from mirp.importData.imageNumpyFile import ImageNumpyFile
@@ -14,5 +16,35 @@ class ImageNumpyFileStack(ImageFileStack):
         super().__init__(image_file_objects, **kwargs)
 
     def complete(self, remove_metadata=True, force=False):
-        # TODO: Order files by file name.
-        ...
+        """
+        Fills out missing attributes in an image stack. Numpy files are actually barebones, and compared to DICOM
+        and many ITK formats lacks the origin. This forces us to determine stacking by the file name.
+        :param remove_metadata: Whether metadata should be removed after completing information.
+        :param force: Whether attributes are forced to update or not.
+        :return: nothing, attributes are updated in place.
+        """
+        # Load metadata of every slice.
+        self.load_metadata()
+
+        self._complete_modality()
+        self._complete_sample_name()
+
+        # Sort by filename.
+        self.sort_image_objects_by_file()
+
+        image_object = copy.deepcopy(self.image_file_objects[0])
+        image_object.complete(remove_metadata=False)
+
+        if self.image_origin is None:
+            self.image_origin = image_object.image_origin
+
+        if self.image_spacing is None:
+            self.image_spacing = image_object.image_spacing
+
+        if self.image_orientation is None:
+            self.image_orientation = image_object.image_orientation
+
+        if self.image_dimension is None:
+            self.image_dimension = tuple([
+                len(self.image_file_objects), image_object.image_dimension[1], image_object.image_dimension[2]
+            ])
