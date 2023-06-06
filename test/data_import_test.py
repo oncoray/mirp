c390f4f3538b68552425df1e3e26b754b1ce3a66
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2023-06-06 11:11:08 +0200

    Generated numpy files for import testing.

--- test/data_import_test.py
+++ test/data_import_test.py
@@ -1,10 +1,91 @@
 import os.path
+
+import itk
+import numpy as np
+
 from mirp.importData.importImage import import_image
 from mirp.importData.importMask import import_mask
 from mirp.importData.importImageAndMask import import_image_and_mask
 
 # Find path to the test directory. This is because we need to read datafiles stored in subdirectories.
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+def _convert_to_numpy(as_slice=False):
+    """
+    Helper script to convert NIfTI files to numpy for testing numpy-based imports.
+
+    :param as_slice:
+    :return:
+    """
+
+    main_folders = ["STS_001", "STS_002", "STS_003"]
+    subfolders = ["CT", "MR_T1", "PET"]
+
+    for sample_name in main_folders:
+        for modality in subfolders:
+
+            # Read and parse image
+            source_image_file = os.path.join(
+                CURRENT_DIR, "data", "sts_images", sample_name, modality, "nifti", "image", "image.nii.gz")
+
+            source_image = itk.imread(source_image_file)
+            source_image = itk.GetArrayFromImage(source_image).astype(np.float32)
+
+            if as_slice:
+                folder_name = "numpy_slice"
+                target_dir = os.path.join(
+                    CURRENT_DIR, "data", "sts_images", sample_name, modality, folder_name, "image")
+
+                if not os.path.exists(target_dir):
+                    os.makedirs(target_dir)
+
+                for ii in range(source_image.shape[0]):
+                    target_image_file = os.path.join(
+                        target_dir, "_".join([sample_name, "{:02d}".format(ii), "image.npy"]))
+
+                    np.save(target_image_file, arr=source_image[ii, :, :].squeeze())
+
+            else:
+                folder_name = "numpy"
+                target_dir = os.path.join(
+                    CURRENT_DIR, "data", "sts_images", sample_name, modality, folder_name, "image")
+                target_image_file = os.path.join(target_dir, "_".join([sample_name, "image.npy"]))
+
+                if not os.path.exists(target_dir):
+                    os.makedirs(target_dir)
+
+                np.save(target_image_file, arr=source_image)
+
+            # Read and parse mask
+            source_mask_file = os.path.join(
+                CURRENT_DIR, "data", "sts_images", sample_name, modality, "nifti", "mask", "mask.nii.gz")
+
+            source_mask = itk.imread(source_mask_file)
+            source_mask = itk.GetArrayFromImage(source_mask).astype(int)
+
+            if as_slice:
+                folder_name = "numpy_slice"
+                target_dir = os.path.join(CURRENT_DIR, "data", "sts_images", sample_name, modality, folder_name, "mask")
+
+                if not os.path.exists(target_dir):
+                    os.makedirs(target_dir)
+
+                for ii in range(source_mask.shape[0]):
+                    target_mask_file = os.path.join(
+                        target_dir, "_".join([sample_name, "{:02d}".format(ii), "mask.npy"]))
+
+                    np.save(target_mask_file, arr=source_mask[ii, :, :].squeeze())
+
+            else:
+                folder_name = "numpy"
+                target_dir = os.path.join(CURRENT_DIR, "data", "sts_images", sample_name, modality, folder_name, "mask")
+                target_mask_file = os.path.join(target_dir, "_".join([sample_name, "mask.npy"]))
+
+                if not os.path.exists(target_dir):
+                    os.makedirs(target_dir)
+
+                np.save(target_mask_file, arr=source_mask)
 
 
 def test_single_image_import():
@@ -34,7 +115,6 @@ def test_single_image_import():
 
     # Read a
     1
-
 
 
 def test_single_image_mask_import():
