670270e7b620c2c97d30e5120e2f9ad9e0a15448
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2023-06-13 12:01:57 +0200

    import_mask now dispatches to _import_image.
    _import_mask is not used.

--- mirp/importData/importMask.py
+++ mirp/importData/importMask.py
@@ -1,70 +1,82 @@
-from functools import singledispatch
+from typing import Union, List
 
-import os.path
-import numpy as np
-import pandas as pd
-
-from mirp.importData.maskGenericFile import MaskFile
-from mirp.importData.maskDirectory import MaskDirectory
+from mirp.importData.imageGenericFile import ImageFile
+from mirp.importData.importImage import _import_image
+from mirp.importData.utilities import supported_mask_modalities, supported_file_types, flatten_list
 
 
-@singledispatch
-def import_mask(mask, **kwargs):
-    raise NotImplementedError(f"Unsupported mask type: {type(mask)}")
+def import_mask(
+        mask,
+        sample_name: Union[None, str, List[str]] = None,
+        mask_name: Union[None, str, List[str]] = None,
+        mask_file_type: Union[None, str] = None,
+        mask_modality: Union[None, str, List[str]] = None,
+        mask_sub_folder: Union[None, str] = None,
+        stack_masks: str = "auto"
+) -> List[ImageFile]:
+    """
+    Creates and curates references to mask files. Masks determine the location of regions of interest. They are
+    usually created by manual, semi-automatic or fully automatic segmentation.
 
+    :param mask: A path to a mask file, a path to a directory containing mask files, a path to a config_data.xml
+    file, a path to a csv file containing references to mask files, a pandas.DataFrame containing references to
+    mask files, or a numpy.ndarray.
+    :param sample_name: Name of expected sample names. This is used to select specific mask files. If None,
+    no mask files are filtered based on the corresponding sample name (if known).
+    :param mask_name: Pattern to match mask files against. The matches are exact. Use wildcard symbols ("*") to
+    match varying structures. The sample name (if part of the file name) can also be specified using "#". For example,
+    mask_name = '#_*_mask' would find John_Doe in John_Doe_CT_mask.nii or John_Doe_001_mask.nii. File extensions
+    do not need to specified. If None, file names are not used for filtering files and setting sample names.
+    :param mask_file_type: The type of file that is expected. If None, the file type is not used for filtering
+    files. Options: "dicom", "nifti", "nrrd", "numpy" and "itk". "itk" comprises "nifti" and "nrrd" file types.
+    :param mask_modality: The type of modality that is expected. If None, modality is not used for filtering files.
+    Note that only DICOM files contain metadata concerning modality. Options: "rtstruct", "seg" or "generic_mask".
+    :param mask_sub_folder: Fixed directory substructure where mask files are located. If None, this directory
+    substructure is not used for filtering files.
+    :param stack_masks: One of auto, yes or no. If mask files in the same directory cannot be assigned to
+    different samples, and are 2D (slices) of the same size, they might belong to the same 3D mask stack. "auto"
+    will stack 2D numpy arrays, but not other file types. "yes" will stack all files that contain 2D images,
+    that have the same dimensions, orientation and spacing, except for DICOM files. "no" will not stack any files.
+    DICOM files ignore this argument, because their stacking can be determined from metadata.
+    :return: list of mask files.
+    """
+    # Check modality.
+    if mask_modality is not None:
+        if not isinstance(mask_modality, str):
+            raise TypeError(
+                f"The mask_modality argument is expected to be a single character string or None. The following "
+                f"modalities are supported: {', '.join(supported_mask_modalities(None))}.")
+        _ = supported_mask_modalities(mask_modality.lower())
 
-@import_mask.register(list)
-def _(mask: list, **kwargs):
-    mask_list = import_mask(
-        mask=mask,
-        **kwargs)
+    # Check image_file_type.
+    if mask_file_type is not None:
+        if not isinstance(mask_file_type, str):
+            raise TypeError(
+                f"The mask_file_type argument is expected to be a single character string, or None. The following file "
+                f"types are supported: {', '.join(supported_file_types(None))}.")
+        _ = supported_file_types(mask_file_type)
+
+    # Check stack_images
+    if stack_masks not in ["yes", "auto", "no"]:
+        raise ValueError(
+            f"The stack_images argument is expected to be one of yes, auto, or no. Found: {stack_masks}."
+        )
+
+    mask_list = _import_image(
+        mask,
+        sample_name=sample_name,
+        image_name=mask_name,
+        image_file_type=mask_file_type,
+        image_modality=mask_modality,
+        image_sub_folder=mask_sub_folder,
+        stack_images=stack_masks,
+        is_mask=True
+    )
+
+    if not isinstance(mask_list, list):
+        mask_list = [mask_list]
+
+    # Flatten list.
+    mask_list = flatten_list(mask_list)
 
     return mask_list
-
-
-@import_mask.register(str)
-def _(mask: str, **kwargs):
-    # Mask is a string, which could be a path to a xml file, to a csv file, or just a regular
-    # path a path to a file, or a path to a directory. Test which it is and then dispatch.
-
-    if mask.lower().endswith("xml"):
-        ...
-
-    elif mask.lower().endswith("csv"):
-        ...
-
-    elif os.path.isdir(mask):
-        return import_mask(
-            MaskDirectory(directory=mask, **kwargs))
-
-    elif os.path.exists(mask):
-        return import_mask(
-            MaskFile(file_path=mask, **kwargs).create())
-
-    else:
-        raise ValueError("The mask path does not point to a xml file, a csv file, a valid image file or a directory "
-                         "containing imaging.")
-
-
-@import_mask.register(pd.DataFrame)
-def _(mask: pd.DataFrame, **kwargs):
-    ...
-
-
-@import_mask.register(np.ndarray)
-def _(mask: np.ndarray, **kwargs):
-    ...
-
-
-@import_mask.register(MaskFile)
-def _(mask: MaskFile, **kwargs):
-    ...
-
-
-@import_mask.register(MaskDirectory)
-def _(mask: MaskDirectory, **kwargs):
-
-    # Check first if the data are consistent.
-    mask.check(raise_error=True)
-
-    # Yield image files
