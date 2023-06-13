6f1bd6fc8444e5d6f8607b295bf32a7ee2414af6
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2023-06-13 12:19:57 +0200

    Argument fixes to mask unit tests.

--- test/import_mask_test.py
+++ test/import_mask_test.py
@@ -13,43 +13,43 @@ def test_single_image_import():
 
     # Read a Nifti image directly.
     mask_list = import_mask(
-        os.path.join(CURRENT_DIR, "data", "sts_images", "STS_001", "CT", "nifti", "image", "image.nii.gz"))
+        os.path.join(CURRENT_DIR, "data", "sts_images", "STS_001", "CT", "nifti", "mask", "mask.nii.gz"))
     assert len(mask_list) == 1
     assert isinstance(mask_list[0], ImageITKFile)
 
     # Read a DICOM image stack.
     mask_list = import_mask(
-        os.path.join(CURRENT_DIR, "data", "sts_images", "STS_001", "CT", "dicom", "image"))
+        os.path.join(CURRENT_DIR, "data", "sts_images", "STS_001", "CT", "dicom", "mask"))
     assert len(mask_list) == 1
     assert isinstance(mask_list[0], ImageDicomFileStack)
     assert mask_list[0].sample_name == "STS_001"
 
     # Read a numpy image directly.
     mask_list = import_mask(
-        os.path.join(CURRENT_DIR, "data", "sts_images", "STS_001", "CT", "numpy", "image", "STS_001_image.npy"))
+        os.path.join(CURRENT_DIR, "data", "sts_images", "STS_001", "CT", "numpy", "mask", "STS_001_mask.npy"))
     assert len(mask_list) == 1
     assert isinstance(mask_list[0], ImageNumpyFile)
 
     # Read a numpy stack.
     mask_list = import_mask(
-        image=os.path.join(CURRENT_DIR, "data", "sts_images", "STS_001", "CT", "numpy_slice", "image"))
+        mask=os.path.join(CURRENT_DIR, "data", "sts_images", "STS_001", "CT", "numpy_slice", "mask"))
     assert len(mask_list) == 1
     assert isinstance(mask_list[0], ImageNumpyFileStack)
 
     # Read a Nifti image for a specific sample.
     mask_list = import_mask(
-        image=os.path.join(CURRENT_DIR, "data", "sts_images"),
+        mask=os.path.join(CURRENT_DIR, "data", "sts_images"),
         sample_name="STS_001",
-        image_sub_folder=os.path.join("CT", "nifti", "image"))
+        mask_sub_folder=os.path.join("CT", "nifti", "mask"))
     assert len(mask_list) == 1
     assert isinstance(mask_list[0], ImageITKFile)
     assert mask_list[0].sample_name == "STS_001"
 
     # Read a DICOM image stack for a specific sample.
     mask_list = import_mask(
-        image=os.path.join(CURRENT_DIR, "data", "sts_images"),
+        mask=os.path.join(CURRENT_DIR, "data", "sts_images"),
         sample_name="STS_001",
-        image_sub_folder=os.path.join("CT", "dicom", "image"))
+        mask_sub_folder=os.path.join("CT", "dicom", "mask"))
     assert len(mask_list) == 1
     assert isinstance(mask_list[0], ImageDicomFileStack)
     assert mask_list[0].sample_name == "STS_001"
@@ -57,49 +57,49 @@ def test_single_image_import():
 
     # Read a numpy image for a specific sample.
     mask_list = import_mask(
-        image=os.path.join(CURRENT_DIR, "data", "sts_images"),
+        mask=os.path.join(CURRENT_DIR, "data", "sts_images"),
         sample_name="STS_001",
-        image_sub_folder=os.path.join("CT", "numpy", "image"))
+        mask_sub_folder=os.path.join("CT", "numpy", "mask"))
     assert len(mask_list) == 1
     assert isinstance(mask_list[0], ImageNumpyFile)
     assert mask_list[0].sample_name == "STS_001"
 
     # Read a numpy image stack for a specific sample.
     mask_list = import_mask(
-        image=os.path.join(CURRENT_DIR, "data", "sts_images"),
+        mask=os.path.join(CURRENT_DIR, "data", "sts_images"),
         sample_name="STS_001",
-        image_sub_folder=os.path.join("CT", "numpy_slice", "image"))
+        mask_sub_folder=os.path.join("CT", "numpy_slice", "mask"))
     assert len(mask_list) == 1
     assert isinstance(mask_list[0], ImageNumpyFileStack)
     assert mask_list[0].sample_name == "STS_001"
 
     # Read a Nifti image by specifying the image name.
     mask_list = import_mask(
-        image=os.path.join(CURRENT_DIR, "data", "sts_images", "STS_001", "CT", "nifti"),
-        image_name="image")
+        mask=os.path.join(CURRENT_DIR, "data", "sts_images", "STS_001", "CT", "nifti"),
+        mask_name="mask")
     assert len(mask_list) == 1
     assert isinstance(mask_list[0], ImageITKFile)
 
     # Read a numpy file by specifying the image name.
     mask_list = import_mask(
-        image=os.path.join(CURRENT_DIR, "data", "sts_images", "STS_001", "CT", "numpy"),
-        image_name="*image")
+        mask=os.path.join(CURRENT_DIR, "data", "sts_images", "STS_001", "CT", "numpy"),
+        mask_name="*mask")
     assert len(mask_list) == 1
     assert isinstance(mask_list[0], ImageNumpyFile)
 
     # Read a numpy stack by specifying the image name.
     mask_list = import_mask(
-        image=os.path.join(CURRENT_DIR, "data", "sts_images", "STS_001", "CT", "numpy_slice"),
-        image_name="*image")
+        mask=os.path.join(CURRENT_DIR, "data", "sts_images", "STS_001", "CT", "numpy_slice"),
+        mask_name="*mask")
     assert len(mask_list) == 1
     assert isinstance(mask_list[0], ImageNumpyFileStack)
 
     # Read a DICOM image stack by specifying the modality, the sample name and the file type.
     mask_list = import_mask(
-        image=os.path.join(CURRENT_DIR, "data", "sts_images"),
+        mask=os.path.join(CURRENT_DIR, "data", "sts_images"),
         sample_name="STS_001",
-        image_modality="rtstruct",
-        image_file_type="dicom")
+        mask_modality="rtstruct",
+        mask_file_type="dicom")
     assert len(mask_list) == 1
     assert isinstance(mask_list[0], ImageDicomFileStack)
     assert mask_list[0].sample_name == "STS_001"
