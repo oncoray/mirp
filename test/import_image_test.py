cb4731954b70325d03b72196f5f0c77bbadae8f6
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2023-06-13 10:25:52 +0200

    Cleaned up tests that are currently not functioning.

--- test/import_image_test.py
+++ test/import_image_test.py
@@ -6,8 +6,6 @@ import numpy as np
 import pytest
 
 from mirp.importData.importImage import import_image
-from mirp.importData.importMask import import_mask
-from mirp.importData.importImageAndMask import import_image_and_mask
 from mirp.importData.imageITKFile import ImageITKFile
 from mirp.importData.imageDicomFileStack import ImageDicomFileStack
 from mirp.importData.imageNumpyFile import ImageNumpyFile
@@ -636,27 +634,3 @@ def test_image_import_flat_poor_naming():
         image_name="*image")
     assert len(image_list) == 3
     assert all(isinstance(image_object, ImageITKFile) for image_object in image_list)
-
-
-def test_single_image_mask_import():
-    import_image_and_mask(
-        image=os.path.join(CURRENT_DIR, "data", "sts_images", "STS_001", "CT", "nifti", "image", "image.nii.gz"),
-        mask=os.path.join(CURRENT_DIR, "data", "sts_images", "STS_001", "CT", "nifti", "mask", "mask.nii.gz"))
-
-    import_image_and_mask(
-        image=os.path.join(CURRENT_DIR, "data", "sts_images", "STS_001", "CT", "dicom", "image"),
-        mask=os.path.join(CURRENT_DIR, "data", "sts_images", "STS_001", "CT", "dicom", "mask"))
-
-    import_image_and_mask(
-        image=os.path.join(CURRENT_DIR, "data", "sts_images"),
-        sample_name="STS_001",
-        image_sub_folder="CT/nifti/image",
-        mask=os.path.join(CURRENT_DIR, "data", "sts_images"),
-        mask_sub_folder="CT/nifti/mask")
-
-    import_image_and_mask(
-        image=os.path.join(CURRENT_DIR, "data", "sts_images"),
-        sample_name="STS_001",
-        image_sub_folder="CT/dicom/image",
-        mask=os.path.join(CURRENT_DIR, "data", "sts_images"),
-        mask_sub_folder="CT/dicom/mask")
