580c3838ab1f7f4b628a428dbc2589d41555c906
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2023-08-15 17:34:50 +0200

    Extended test_read_itk_image_and_mask unit tests.

--- test/read_image_and_mask_test.py
+++ test/read_image_and_mask_test.py
@@ -19,6 +19,7 @@ def test_read_itk_image_and_mask():
 
     image, roi_list = read_image_and_masks(image=image_list[0])
     assert isinstance(image, ImageClass)
+    assert len(roi_list) == 1
     assert all(isinstance(roi, RoiClass) for roi in roi_list)
 
     # With roi name specified.
@@ -30,6 +31,7 @@ def test_read_itk_image_and_mask():
 
     image, roi_list = read_image_and_masks(image=image_list[0])
     assert isinstance(image, ImageClass)
+    assert len(roi_list) == 1
     assert all(isinstance(roi, RoiClass) for roi in roi_list)
 
     # With roi name not appearing.
@@ -42,3 +44,30 @@ def test_read_itk_image_and_mask():
     image, roi_list = read_image_and_masks(image=image_list[0])
     assert isinstance(image, ImageClass)
     assert len(roi_list) == 0
+
+    # Multiple roi names of which one is present.
+    image_list = import_image_and_mask(
+        image=os.path.join(CURRENT_DIR, "data", "sts_images", "STS_001", "CT", "nifti", "image", "image.nii.gz"),
+        mask=os.path.join(CURRENT_DIR, "data", "sts_images", "STS_001", "CT", "nifti", "mask", "mask.nii.gz"),
+        roi_name=["1", "2", "3"]
+    )
+
+    image, roi_list = read_image_and_masks(image=image_list[0])
+    assert isinstance(image, ImageClass)
+    assert len(roi_list) == 1
+    assert all(isinstance(roi, RoiClass) for roi in roi_list)
+
+    # Multiple roi names, with dictionary to set labels.
+    image_list = import_image_and_mask(
+        image=os.path.join(CURRENT_DIR, "data", "sts_images", "STS_001", "CT", "nifti", "image", "image.nii.gz"),
+        mask=os.path.join(CURRENT_DIR, "data", "sts_images", "STS_001", "CT", "nifti", "mask", "mask.nii.gz"),
+        roi_name={"1": "gtv", "2": "some_roi", "3": "another_roi"}
+    )
+
+    image, roi_list = read_image_and_masks(image=image_list[0])
+    assert isinstance(image, ImageClass)
+    assert len(roi_list) == 1
+    assert all(isinstance(roi, RoiClass) for roi in roi_list)
+    assert roi_list[0].name == "gtv"
+
+
