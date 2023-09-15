de8b444e52276637fc670653ea469c7038f71a96
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2023-09-15 12:13:16 +0200

    Removed redundant code.

--- mirp/utilities.py
+++ mirp/utilities.py
@@ -1,33 +1,5 @@
 import os
-from collections import Counter
-
 import numpy as np
-
-
-def parse_roi_name(roi: str):
-
-    # Determine if the input roi is surrounded by curly brackets
-    if roi.startswith("{") and roi.endswith("}"):
-        # Strip curly brackets
-        roi = roi.strip("{}")
-
-        # Get individual rois. & and | are valid splitting variables.
-        if "&" in roi:
-            indiv_roi = roi.split("&")
-
-        elif "|" in roi:
-            indiv_roi = roi.split("|")
-
-        else:
-            indiv_roi = [roi]
-
-        # Remove white space around individual rois
-        indiv_roi = [curr_roi.strip() for curr_roi in indiv_roi]
-
-    else:
-        indiv_roi = [roi.strip()]
-
-    return indiv_roi
 
 
 def check_string(input_string):
@@ -44,23 +16,6 @@ def check_string(input_string):
     input_string = input_string.replace("|", "_or_")
 
     return input_string
-
-
-def get_valid_elements(input_list):
-
-    filt = [elem_ii is not None for elem_ii in input_list]
-
-    return [elem_ii for (elem_ii, flag) in zip(input_list, filt) if flag]
-
-
-def get_most_common_element(input_list):
-
-    if len(input_list) == 0:
-        return None
-
-    counts = Counter(input_list)
-
-    return counts.most_common(n=1)[0][0]
 
 
 def get_version():
