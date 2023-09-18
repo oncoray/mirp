59f8dfd4ccb1434c51b409ccc1a5e1aec0b88095
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2023-09-18 16:01:17 +0200

    Added tests for modality-specific file import.

--- test/read_image_and_mask_test.py
+++ test/read_image_and_mask_test.py
@@ -4,6 +4,9 @@ from mirp.importData.importImageAndMask import import_image_and_mask
 from mirp.importData.readData import read_image_and_masks
 from mirp.images.genericImage import GenericImage
 from mirp.masks.baseMask import BaseMask
+from mirp.images.ctImage import CTImage
+from mirp.images.petImage import PETImage
+from mirp.images.mrImage import MRImage
 
 
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -255,3 +258,82 @@ def test_read_dicom_image_and_mask_stack():
     assert len(roi_list) == 1
     assert all(isinstance(roi, BaseMask) for roi in roi_list)
     assert roi_list[0].roi_name == "gtv"
+
+
+def test_read_dicom_image_and_mask_modality_specific():
+    # Read CT image.
+    image_list = import_image_and_mask(
+        image=os.path.join(CURRENT_DIR, "data", "sts_images", "STS_001", "CT", "dicom", "image"),
+        mask=os.path.join(CURRENT_DIR, "data", "sts_images", "STS_001", "CT", "dicom", "mask", "RS.dcm")
+    )
+
+    image, roi_list = read_image_and_masks(image=image_list[0])
+    assert isinstance(image, CTImage)
+    assert len(roi_list) == 1
+    assert all(isinstance(roi, BaseMask) for roi in roi_list)
+    assert roi_list[0].roi_name == "GTV_Mass_CT"
+
+    # Read PET image.
+    image_list = import_image_and_mask(
+        image=os.path.join(CURRENT_DIR, "data", "sts_images", "STS_001", "PET", "dicom", "image"),
+        mask=os.path.join(CURRENT_DIR, "data", "sts_images", "STS_001", "PET", "dicom", "mask", "RS.dcm")
+    )
+
+    image, roi_list = read_image_and_masks(image=image_list[0])
+    assert isinstance(image, PETImage)
+    assert len(roi_list) == 1
+    assert all(isinstance(roi, BaseMask) for roi in roi_list)
+    assert roi_list[0].roi_name == "GTV_Mass_PET"
+
+    # Read T1-weighted MR image.
+    image_list = import_image_and_mask(
+        image=os.path.join(CURRENT_DIR, "data", "sts_images", "STS_001", "MR_T1", "dicom", "image"),
+        mask=os.path.join(CURRENT_DIR, "data", "sts_images", "STS_001", "MR_T1", "dicom", "mask", "RS.dcm")
+    )
+
+    image, roi_list = read_image_and_masks(image=image_list[0])
+    assert isinstance(image, MRImage)
+    assert len(roi_list) == 1
+    assert all(isinstance(roi, BaseMask) for roi in roi_list)
+    assert roi_list[0].roi_name == "GTV_Mass_MR_T1"
+
+
+def test_read_generic_image_and_mask_modality_specific():
+    # Read CT image.
+    image_list = import_image_and_mask(
+        image=os.path.join(CURRENT_DIR, "data", "sts_images", "STS_001", "CT", "nifti", "image", "image.nii.gz"),
+        mask=os.path.join(CURRENT_DIR, "data", "sts_images", "STS_001", "CT", "nifti", "mask", "mask.nii.gz"),
+        image_modality="CT"
+    )
+
+    image, roi_list = read_image_and_masks(image=image_list[0])
+    assert isinstance(image, CTImage)
+    assert len(roi_list) == 1
+    assert all(isinstance(roi, BaseMask) for roi in roi_list)
+    assert roi_list[0].roi_name == "region_1"
+
+    # Read PET image.
+    image_list = import_image_and_mask(
+        image=os.path.join(CURRENT_DIR, "data", "sts_images", "STS_001", "PET", "nifti", "image", "image.nii.gz"),
+        mask=os.path.join(CURRENT_DIR, "data", "sts_images", "STS_001", "PET", "nifti", "mask", "mask.nii.gz"),
+        image_modality="PET"
+    )
+
+    image, roi_list = read_image_and_masks(image=image_list[0])
+    assert isinstance(image, PETImage)
+    assert len(roi_list) == 1
+    assert all(isinstance(roi, BaseMask) for roi in roi_list)
+    assert roi_list[0].roi_name == "region_1"
+
+    # Read T1-weighted MR image.
+    image_list = import_image_and_mask(
+        image=os.path.join(CURRENT_DIR, "data", "sts_images", "STS_001", "MR_T1", "nifti", "image", "image.nii.gz"),
+        mask=os.path.join(CURRENT_DIR, "data", "sts_images", "STS_001", "MR_T1", "nifti", "mask", "mask.nii.gz"),
+        image_modality="MR"
+    )
+
+    image, roi_list = read_image_and_masks(image=image_list[0])
+    assert isinstance(image, MRImage)
+    assert len(roi_list) == 1
+    assert all(isinstance(roi, BaseMask) for roi in roi_list)
+    assert roi_list[0].roi_name == "region_1"
