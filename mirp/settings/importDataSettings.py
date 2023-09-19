241fc94bdde5a610b266f14c45e286775635fe71
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2023-09-19 16:23:40 +0200

    Removed deprecated xml tags and added deprecation warnings.

--- mirp/settings/importDataSettings.py
+++ mirp/settings/importDataSettings.py
@@ -1,11 +1,13 @@
 import copy
 import os
+import warnings
 
 from xml.etree import ElementTree as ElemTree
 
 from mirp.settings.utilities import str2list, str2type
 
 
+# noinspection PyDeprecation
 def import_data_settings(
     path: str,
     is_mask: bool
@@ -41,12 +43,15 @@ def import_data_settings(
         sample_name = str2list(paths_branch.find("subject_include"), "str")
         data_arguments += [("sample_name", sample_name)]
 
-        # Deprecated stuff.
-        # TODO: Add deprecation warnings.
-        # excl_subj = str2list(paths_branch.find("subject_exclude"), "str")
-        # write_path = os.path.normpath(str2type(paths_branch.find("write_folder"), "path"))
-        # provide_diagnostics = str2type(paths_branch.find("provide_diagnostics"), "bool")
-        # cohort_id = str2type(paths_branch.find("cohort"), "str", "NA")
+        # Deprecated items.
+        if paths_branch.find("subject_exclude") is not None:
+            _import_data_settings_deprecation_warning("subject_exclude")
+        if paths_branch.find("write_folder") is not None:
+            _import_data_settings_deprecation_warning("write_path")
+        if paths_branch.find("provide_diagnostics") is not None:
+            _import_data_settings_deprecation_warning("provide_diagnostics")
+        if paths_branch.find("cohort") is not None:
+            _import_data_settings_deprecation_warning("cohort")
        
         # Iterate over data branches
         for data_branch in branch.findall("data"):
@@ -69,15 +74,25 @@ def import_data_settings(
                 image_name = str2type(data_branch.find("image_filename_pattern"), "str")
                 current_data_arguments += [("image_name", image_name)]
 
-            # More deprecated stuff.
-            # TODO: add deprecation warnings.
-            # roi_reg_img_folder = str2type(data_branch.find("registration_image_folder"), "str")
-            # registration_image_file_name_pattern = str2type(
-            #     data_branch.find("registration_image_filename_pattern"), "str")
-            # roi_list_path: str = str2type(data_branch.find("roi_list_path"), "str")
-            # divide_disconnected_roi = str2type(data_branch.find("divide_disconnected_roi"), "str", "combine")
-            # extraction_config = str2list(data_branch.find("extraction_config"), "str")
+            # More deprecated items.
+            if data_branch.find("registration_image_folder") is not None:
+                _import_data_settings_deprecation_warning("registration_image_folder")
+            if data_branch.find("registration_image_filename_pattern") is not None:
+                _import_data_settings_deprecation_warning("registration_image_filename_pattern")
+            if data_branch.find("roi_list_path") is not None:
+                _import_data_settings_deprecation_warning("roi_list_path")
+            if data_branch.find("divide_disconnected_roi") is not None:
+                _import_data_settings_deprecation_warning("divide_disconnected_roi")
+            if data_branch.find("extraction_config") is not None:
+                _import_data_settings_deprecation_warning("extraction_config")
 
             data_arguments_list += [current_data_arguments]
 
     return data_arguments_list
+
+
+def _import_data_settings_deprecation_warning(tag: str) -> None:
+    warnings.warn(
+        f"The {tag} tag was deprecated in version 2.0, and will not be parsed.",
+        DeprecationWarning
+    )
