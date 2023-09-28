6cb814df5df859fb4088e0e84505406e99974fdc
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2023-09-28 17:35:14 +0200

    Renamed n_cpu to num_cpus to match the variable name used in the ray package.

--- mirp/utilities/rayUtilities.py
+++ mirp/utilities/rayUtilities.py
@@ -1,14 +1,14 @@
 import ray
 
 
-def initialise_ray(n_cpu: None | int = None):
+def initialise_ray(num_cpus: None | int = None):
     """
     Initialises (local) ray cluster. A ray cluster may also be specified by the user, in which case no new cluster is
     started.
 
     Parameters
     ----------
-    n_cpu: int, optional, default: None
+    num_cpus: int, optional, default: None
         Number of (virtual) CPU nodes available or requested. `n_cpu = None` prevents starting a local ray cluster.
 
     Returns
@@ -19,12 +19,12 @@ def initialise_ray(n_cpu: None | int = None):
     if ray.is_initialized():
         # Prevent replacing any user-specified ray cluster.
         pass
-    elif n_cpu is None or n_cpu < 2:
+    elif num_cpus is None or num_cpus < 2:
         # Prevent starting a ray cluster where none is required.
         pass
     else:
         # Start a local cluster with the desired number
-        ray.init(num_cpus=n_cpu)
+        ray.init(num_cpus=num_cpus)
 
 
 def use_ray_remote() -> bool:
