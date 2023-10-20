80e6ecd99aede174bc52dbeaf0244fd59a630d76
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2023-10-20 10:24:21 +0200

    Implemented function for copy data xml files to a local directory.

--- mirp/utilities/config_utilities.py
+++ mirp/utilities/config_utilities.py
@@ -42,28 +42,38 @@ def get_settings_xml(target_dir: str):
     print(f"A copy of the settings xml file was created at {destination_file_path}.")
 
 
+def get_data_xml(target_dir: str):
+    """
+    Creates a local copy of the data ``xml`` file. This file can be used to configure import of images and masks.
+
     Parameters
     ----------
     target_dir: str
-        Path where the configuration xml file should be copied to.
+        Path where the data ``xml`` file should be copied to.
 
     Returns
     -------
     None
-        The configuration file is copied to the intended directory.
+        No return values. The data ``xml`` is copied to the intended directory.
     """
 
     if not os.path.exists(target_dir):
         os.makedirs(target_dir)
 
-    destination_file_path = os.path.join(target_dir, "configuration.xml")
+    destination_file_path = os.path.join(target_dir, "data.xml")
     if os.path.exists(destination_file_path):
         warnings.warn(
-            f"A configuration file already exists at {destination_file_path}.", UserWarning
+            f"A data xml file already exists at {destination_file_path}.", UserWarning
         )
         return
 
-    mirp_dir = sys.modules["mirp"].__path__[0]
-    source_file_path = os.path.join(mirp_dir, "config.xml")
+    # mirp might not be formally installed as a module.
+    try:
+        mirp_dir = sys.modules["mirp"].__path__[0]
+    except KeyError:
+        mirp_dir = os.path.dirname(os.path.abspath(os.path.join(__file__, os.path.pardir)))
+    source_file_path = os.path.join(mirp_dir, "config_data.xml")
+
     shutil.copy(source_file_path, destination_file_path)
 
+    print(f"A copy of the data xml file was created at {destination_file_path}.")
--- test/xml_file_copy_test.py
+++ test/xml_file_copy_test.py
@@ -1,11 +1,10 @@
 import os
-from mirp.utilities.config_utilities import get_settings_xml
+from mirp.utilities.config_utilities import get_settings_xml, get_data_xml
 
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 def test_copy_settings_xml():
-
     target_dir = os.path.join(CURRENT_DIR, "data", "temp")
     target_file = os.path.join(target_dir, "settings.xml")
 
@@ -14,6 +13,22 @@ def test_copy_settings_xml():
         os.remove(target_file)
 
     get_settings_xml(target_dir=target_dir)
+
+    assert os.path.exists(target_file)
+
+    # Clean up.
+    os.remove(target_file)
+
+
+def test_copy_data_xml():
+    target_dir = os.path.join(CURRENT_DIR, "data", "temp")
+    target_file = os.path.join(target_dir, "data.xml")
+
+    # Start with a clean slate.
+    if os.path.exists(target_file):
+        os.remove(target_file)
+
+    get_data_xml(target_dir=target_dir)
 
     assert os.path.exists(target_file)
 
