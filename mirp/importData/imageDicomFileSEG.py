267b416ee3611dbba491cc9da669a5a01bbd3d4f
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2023-11-17 16:02:51 +0100

    Added SEG to import_mask_test.

--- mirp/importData/imageDicomFileSEG.py
+++ mirp/importData/imageDicomFileSEG.py
@@ -1,4 +1,7 @@
+from typing import Optional, List
+
 from mirp.importData.imageDicomFile import MaskDicomFile
+from mirp.masks.baseMask import BaseMask
 
 
 class MaskDicomFileSEG(MaskDicomFile):
@@ -7,5 +10,26 @@ class MaskDicomFileSEG(MaskDicomFile):
 
         super().__init__(**kwargs)
 
+    def is_stackable(self, stack_images: str):
+        return False
+
+    def create(self):
+        return self
+
+    def _complete_image_origin(self, force=False):
+        return
+
+    def _complete_image_orientation(self, force=False):
+        return
+
+    def _complete_image_spacing(self, force=False):
+        return
+
+    def _complete_image_dimensions(self, force=False):
+        return
+
     def load_data(self, **kwargs):
+        ...
+
+    def to_object(self, **kwargs) -> Optional[List[BaseMask]]:
         ...
