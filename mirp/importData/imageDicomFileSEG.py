902b60fef686548aae16a04e820f8af09bf29aa3
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2023-11-20 17:25:57 +0100

    Added code to pass import_image_and_mask_test unit tests.

--- mirp/importData/imageDicomFileSEG.py
+++ mirp/importData/imageDicomFileSEG.py
@@ -1,4 +1,5 @@
 from typing import Optional, List
+import warnings
 
 from mirp.importData.imageDicomFile import MaskDicomFile
 from mirp.importData.utilities import get_pydicom_meta_tag, has_pydicom_meta_tag
@@ -29,8 +30,39 @@ class MaskDicomFileSEG(MaskDicomFile):
     def _complete_image_dimensions(self, force=False):
         return
 
+    def check_mask(self, raise_error=True):
+        if self.image_metadata is None:
+            raise TypeError("DEV: the image_meta_data attribute has not been set.")
+
+        if not get_pydicom_meta_tag(dcm_seq=self.image_metadata, tag=(0x0062, 0x0002), test_tag=True):
+            if raise_error:
+                warnings.warn(
+                    f"The SEG set ({self.file_path}) did not contain any segment sequences.",
+                )
+            return False
+
+        roi_name_present = [
+            get_pydicom_meta_tag(
+                dcm_seq=current_segment_sequence, tag=(0x0062, 0x0005), tag_type="str", default=None)
+            for current_segment_sequence in self.image_metadata[(0x0062, 0x0002)]
+        ]
+
+        if len(roi_name_present) == 0 and raise_error:
+            warnings.warn(
+                f"The current SEG set ({self.file_path}) does not contain any segmentations."
+            )
+            return False
+
+        if any(x is None for x in roi_name_present) and raise_error:
+            warnings.warn(
+                f"The current SEG set ({self.file_path}) does not contain any labels."
+            )
+            return False
+
+        return True
+
     def load_data(self, **kwargs):
-        ...
+        pass
 
     def to_object(self, **kwargs) -> Optional[List[BaseMask]]:
         ...
