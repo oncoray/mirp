f003f26e6d46bb002c8b564449ef65b6fbc5e248
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2025-07-02 17:28:22 +0200

    Update version.

--- docs_source/source/conf.py
+++ docs_source/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2025, Alex Zwanenburg'
 author = 'Alex Zwanenburg'
 
 # The full version, including alpha/beta/rc tags
-release = '2.3.3'
+release = '2.3.4'
 
 
 # -- General configuration ---------------------------------------------------
