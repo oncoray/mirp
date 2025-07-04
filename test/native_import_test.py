0550b1271f560b3341ca604ef76eca5047ff6b61
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2025-07-04 17:51:48 +0200

    Unit test for importing multiple native image and mask objects.

--- test/native_import_test.py
+++ test/native_import_test.py
@@ -43,3 +43,39 @@ def test_import_native_single_image():
     assert np.array_equal(image.get_voxel_grid(), new_image.get_voxel_grid())
     assert np.array_equal(mask.roi.get_voxel_grid(), new_mask.roi.get_voxel_grid())
     assert feature_data.equals(new_feature_data)
+
+def test_import_native_multiple_images():
+
+    data = extract_features_and_images(
+        image_export_format="native",
+        image=os.path.join(CURRENT_DIR, "data", "sts_images"),
+        image_sub_folder=os.path.join("CT", "nifti", "image"),
+        mask_sub_folder=os.path.join("CT", "nifti", "mask"),
+        image_modality="CT",
+        base_feature_families = "statistics"
+    )
+
+    feature_data = [x[0] for x in data]
+    image = [x[1][0] for x in data]
+    mask = [x[2][0] for x in data]
+
+    assert all(isinstance(x, CTImage) for x in image)
+    assert all(isinstance(x, BaseMask) for x in mask)
+
+    data = extract_features_and_images(
+        image_export_format="native",
+        image = image,
+        mask = mask,
+        base_feature_families = "statistics"
+    )
+
+    new_feature_data = [x[0] for x in data]
+    new_image = [x[1][0] for x in data]
+    new_mask = [x[2][0] for x in data]
+
+    for ii, old_feature_data in enumerate(feature_data):
+        assert old_feature_data.equals(new_feature_data[ii])
+    for ii, old_image in enumerate(image):
+        assert np.array_equal(old_image.get_voxel_grid(), new_image[ii].get_voxel_grid)
+    for ii, old_mask in enumerate(mask):
+        assert np.array_equal(old_mask.roi.get_voxel_grid(), new_mask[ii].roi.get_voxel_grid)
