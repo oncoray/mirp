e820e725e7e903c53d0550ae5aa6699d2391d971
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2025-07-06 22:28:21 +0200

    Add unit tests extracting metadata from native image objects..

--- test/native_import_test.py
+++ test/native_import_test.py
@@ -5,8 +5,7 @@ import pytest
 
 from mirp._images.ct_image import CTImage
 from mirp._masks.base_mask import BaseMask
-from mirp.extract_features_and_images import extract_features_and_images
-from mirp.extract_mask_labels import extract_mask_labels
+from mirp import extract_features_and_images, extract_mask_labels, extract_image_parameters
 
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -97,7 +96,6 @@ def test_extract_labels_native_multiple():
         base_feature_families="statistics"
     )
 
-    feature_data = [x[0] for x in data]
     image = [x[1][0] for x in data]
     mask = [x[2][0] for x in data]
 
@@ -112,4 +110,54 @@ def test_extract_labels_native_multiple():
     assert len(roi_labels["roi_label"]) == 3
     assert all(roi_label == "GTV_Mass_CT" for roi_label in roi_labels["roi_label"])
 
-# TODO: add test for extract_image_parameters
+
+def test_extract_metadata_parameter_native_multiple_dicom():
+    data = extract_features_and_images(
+        image_export_format="native",
+        image=os.path.join(CURRENT_DIR, "data", "sts_images"),
+        image_sub_folder=os.path.join("CT", "dicom", "image"),
+        mask_sub_folder=os.path.join("CT", "dicom", "mask"),
+        image_modality="CT",
+        base_feature_families="statistics"
+    )
+
+    image = [x[1][0] for x in data]
+    mask = [x[2][0] for x in data]
+
+    assert all(isinstance(x, CTImage) for x in image)
+    assert all(isinstance(x, BaseMask) for x in mask)
+    assert all(x.sample_name in ["STS_001", "STS_002", "STS_003"] for x in image)
+    assert all(x.sample_name in ["STS_001", "STS_002", "STS_003"] for x in mask)
+
+    metadata = extract_image_parameters(
+        image=image
+    )
+
+    assert np.array_equal(metadata["kvp"].values, np.array([140.0, 140.0, 120.0]))
+    assert len(metadata["kvp"]) == 3
+
+
+def test_extract_metadata_parameter_native_multiple_non_dicom():
+    data = extract_features_and_images(
+        image_export_format="native",
+        image=os.path.join(CURRENT_DIR, "data", "sts_images"),
+        image_sub_folder=os.path.join("CT", "nifti", "image"),
+        mask_sub_folder=os.path.join("CT", "nifti", "mask"),
+        image_modality="CT",
+        base_feature_families="statistics"
+    )
+
+    image = [x[1][0] for x in data]
+    mask = [x[2][0] for x in data]
+
+    assert all(isinstance(x, CTImage) for x in image)
+    assert all(isinstance(x, BaseMask) for x in mask)
+    assert all(x.sample_name in ["STS_001", "STS_002", "STS_003"] for x in image)
+    assert all(x.sample_name in ["STS_001", "STS_002", "STS_003"] for x in mask)
+
+    metadata = extract_image_parameters(
+        image=image
+    )
+
+    assert all(x == "nifti" for x in metadata["file_type"].values)
+    assert len(metadata["file_type"]) == 3
