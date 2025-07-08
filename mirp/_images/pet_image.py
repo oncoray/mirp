66f9be7beecadf386c767b4dca3b9cae59768590
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2025-07-08 12:06:08 +0200

    For scaling and normalising intensities of CT images, generate GenericImage first, and then process.

--- mirp/_images/pet_image.py
+++ mirp/_images/pet_image.py
@@ -42,29 +42,25 @@ class PETImage(GenericImage):
         Normalise intensities. NOTE: this changes the class of the object from PETImage to GenericImage as
         normalisation breaks the one-to-one relationship between intensities and SUV values or PET counts.
         """
-        image = super().normalise_intensities(
+        if self.image_data is None:
+            return self
+
+        if normalisation_method is None or normalisation_method == "none":
+            return self
+
+        new_image = GenericImage(image_data=self.image_data)
+        new_image.update_from_template(template=self)
+        new_image = new_image.normalise_intensities(
             normalisation_method=normalisation_method,
             intensity_range=intensity_range,
             saturation_range=saturation_range,
             mask=mask
         )
 
-        if image.image_data is None:
-            return self
-
-        if normalisation_method is None or normalisation_method == "none":
-            return self
-
-        new_image = GenericImage(image_data=image.image_data)
-        new_image.update_from_template(template=image)
-
         return new_image
 
     def scale_intensities(self, scale: float):
-
-        image = super().scale_intensities(scale=scale)
-
-        if image.image_data is None:
+        if self.image_data is None:
             return self
 
         if scale == 1.0:
@@ -72,7 +68,8 @@ class PETImage(GenericImage):
 
         # Scaling intensities changes the object class from PETImage to GenericImage as this breaks the one-to-one
         # relationship between intensities and SUV values or PET counts.
-        new_image = GenericImage(image_data=image.image_data)
-        new_image.update_from_template(template=image)
+        new_image = GenericImage(image_data=self.image_data)
+        new_image.update_from_template(template=self)
+        new_image = new_image.scale_intensities(scale=scale)
 
         return new_image
