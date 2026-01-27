dceeb0f376f4c70a2071f67f9f03d362eef82d3d
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2026-01-27 11:55:20 +0100

    Add tests.

--- test/intensity_normalisation_test.py
+++ test/intensity_normalisation_test.py
@@ -1,4 +1,5 @@
 import os.path
+import numpy as np
 
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -14,12 +15,13 @@ GENERIC_KWARGS = dict([
     ("base_feature_families","statistics")
 ])
 
+
 def test_intensity_normalisation():
     from mirp.settings.image_processing_parameters import ImagePostProcessingClass
     from mirp import extract_features_and_images
     from mirp._masks.base_mask import BaseMask
     from mirp._images.generic_image import GenericImage
-    from mirp._images.ct_image import CTImage
+    from mirp._images.mr_image import MRImage
 
     for intensity_normalisation_method in ImagePostProcessingClass()._get_available_intensity_normalisation_methods():
         # Skip over select intensity normalisation methods that require a custom test.
@@ -52,19 +54,64 @@ def test_intensity_normalisation():
         assert len(feature_data_m) == 1
         assert isinstance(mask_nm, BaseMask)
         assert isinstance(mask_m, BaseMask)
+        assert isinstance(image_nm, MRImage)
+        assert isinstance(image_m, MRImage)
 
         if intensity_normalisation_method == "none":
-            assert isinstance(image_nm, CTImage)
-            assert isinstance(image_m, CTImage)
-        else:
-            assert isinstance(image_nm, GenericImage)
-            assert isinstance(image_m, GenericImage)
+            assert np.min(image_nm.get_voxel_grid()) == 0.0
+            assert np.max(image_nm.get_voxel_grid()) == 1807.0
+            assert np.min(image_m.get_voxel_grid()) == 0.0
+            assert np.max(image_m.get_voxel_grid()) == 1807.0
+            assert 239.0 < np.std(image_nm.get_voxel_grid()) < 240.0
+            assert 239.0 < np.std(image_nm.get_voxel_grid()) < 240.0
 
-        if intensity_normalisation_method == "none":
-            assert feature_data_nm["stat_max"].to_numpy()[0] == 350.0
-            assert feature_data_m["stat_max"].to_numpy()[0] == 350.0
-            assert feature_data_nm["stat_min"].to_numpy()[0] == 0.0
-            assert feature_data_m["stat_min"].to_numpy()[0] == 0.0
+        elif intensity_normalisation_method == "range":
+            assert np.min(image_nm.get_voxel_grid()) == 0.0
+            assert np.max(image_nm.get_voxel_grid()) == 1.0
+            assert -0.05 < np.min(image_m.get_voxel_grid()) < 0.0
+            assert np.max(image_m.get_voxel_grid()) == 1.0
+            assert 0.13 < np.std(image_nm.get_voxel_grid()) < 0.14
+            assert 0.13 < np.std(image_m.get_voxel_grid()) < 0.14
+
+        elif intensity_normalisation_method == "relative_range":
+            assert np.min(image_nm.get_voxel_grid()) == 0.0
+            assert np.max(image_nm.get_voxel_grid()) == 1.0
+            assert -0.05 < np.min(image_m.get_voxel_grid()) < 0.0
+            assert np.max(image_m.get_voxel_grid()) == 1.0
+            assert 0.13 < np.std(image_nm.get_voxel_grid()) < 0.14
+            assert 0.13 < np.std(image_m.get_voxel_grid()) < 0.14
+
+        elif intensity_normalisation_method == "quantile_range":
+            assert np.min(image_nm.get_voxel_grid()) < 0.0
+            assert np.max(image_nm.get_voxel_grid()) > 2.0
+            assert np.min(image_m.get_voxel_grid()) < 0.0
+            assert np.max(image_m.get_voxel_grid()) > 2.0
+            assert 0.29 < np.std(image_nm.get_voxel_grid()) < 0.30
+            assert 0.29 < np.std(image_m.get_voxel_grid()) < 0.30
+
+        elif intensity_normalisation_method == "standardisation":
+            assert -1.1 < np.min(image_nm.get_voxel_grid()) < -1.0
+            assert 6.4 < np.max(image_nm.get_voxel_grid()) < 6.5
+            assert -1.4 < np.min(image_m.get_voxel_grid()) < -1.3
+            assert 6.3 < np.max(image_m.get_voxel_grid()) < 6.4
+            assert 0.99 < np.std(image_nm.get_voxel_grid()) < 1.00
+            assert 1.00 < np.std(image_m.get_voxel_grid()) < 1.01
+
+        elif intensity_normalisation_method == "histogram_equalisation":
+            assert np.min(image_nm.get_voxel_grid()) == 0.0
+            assert np.max(image_nm.get_voxel_grid()) == 1.0
+            assert np.min(image_m.get_voxel_grid()) == 0.0
+            assert np.max(image_m.get_voxel_grid()) == 1.0
+            assert 0.27 < np.std(image_nm.get_voxel_grid()) < 0.28
+            assert 0.32 < np.std(image_m.get_voxel_grid()) < 0.33
+
+        elif intensity_normalisation_method == "adaptive_equalisation":
+            assert np.min(image_nm.get_voxel_grid()) == 0.0
+            assert np.max(image_nm.get_voxel_grid()) == 1.0
+            assert np.min(image_m.get_voxel_grid()) == 0.0
+            assert np.max(image_m.get_voxel_grid()) == 1.0
+            assert 0.27 < np.std(image_nm.get_voxel_grid()) < 0.28
+            assert 0.32 < np.std(image_m.get_voxel_grid()) < 0.33
 
         else:
             raise NotImplementedError(f"Test for intensity_normalisation_method {intensity_normalisation_method} not implemented")
