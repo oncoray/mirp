e863efc7e628d5fffb5b6d5aac7cd8a22cbf0449
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2026-01-27 15:52:53 +0100

    Add tests for reference matching.

--- test/intensity_normalisation_test.py
+++ test/intensity_normalisation_test.py
@@ -170,6 +170,7 @@ def test_custom_scale():
     assert isinstance(image_nm, MRImage)
     assert isinstance(image_m, MRImage)
 
+    # Mask should not have an effect.
     assert np.min(image_nm.get_voxel_grid()) == -5.0
     assert 13.0 < np.max(image_nm.get_voxel_grid()) < 13.1
     assert np.min(image_m.get_voxel_grid()) == -5.0
@@ -179,4 +180,106 @@ def test_custom_scale():
 
 
 def test_match_reference():
-    ...
+    from mirp import extract_features_and_images, extract_images
+    from mirp._images.mr_image import MRImage
+    from mirp._images.pet_image import PETImage
+    from mirp._masks.base_mask import BaseMask
+
+    # Reference image (FDG-PET)
+    data_ref = extract_images(
+        write_images=False,
+        export_images=True,
+        image_export_format="native",
+        image=os.path.join(CURRENT_DIR, "data", "sts_images_raw", "STS_001", "FDG_PET", "image"),
+        mask=os.path.join(CURRENT_DIR, "data", "sts_images_raw", "STS_001", "FDG_PET", "mask"),
+        roi_name="GTV_Mass"
+    )
+
+    image_ref = data_ref[0][0][0]
+    assert isinstance(image_ref, PETImage)
+    image_ref = image_ref.get_voxel_grid()
+
+    # No tissue mask.
+    data_no_mask = extract_features_and_images(
+        tissue_mask_type="none",
+        intensity_normalisation="match_reference",
+        intensity_normalisation_reference=image_ref,
+        **GENERIC_KWARGS
+    )
+
+    # Tissue mask
+    data_mask = extract_features_and_images(
+        tissue_mask_type="relative_range",
+        tissue_mask_range=[0.02, 1.00],
+        intensity_normalisation="match_reference",
+        intensity_normalisation_reference=image_ref,
+        **GENERIC_KWARGS
+    )
+
+    feature_data_nm = data_no_mask[0][0]
+    image_nm = data_no_mask[0][1][0]
+    mask_nm = data_no_mask[0][2][0]
+    feature_data_m = data_mask[0][0]
+    image_m = data_mask[0][1][0]
+    mask_m = data_mask[0][2][0]
+
+    assert len(feature_data_nm) == 1
+    assert len(feature_data_m) == 1
+    assert isinstance(mask_nm, BaseMask)
+    assert isinstance(mask_m, BaseMask)
+    assert isinstance(image_nm, MRImage)
+    assert isinstance(image_m, MRImage)
+
+    assert np.min(image_nm.get_voxel_grid()) == np.min(image_ref)
+    assert np.max(image_ref) - 0.01 < np.max(image_nm.get_voxel_grid()) <= np.max(image_ref)
+    assert np.min(image_m.get_voxel_grid()) == np.min(image_ref)
+    assert np.max(image_ref) - 0.01 < np.max(image_m.get_voxel_grid()) <= np.max(image_ref)
+    assert 0.657 < np.std(image_nm.get_voxel_grid()) < 0.658
+    assert 0.589 < np.std(image_m.get_voxel_grid()) < 0.590
+
+    # The no mask image should have comparable variance and quantiles.
+    q_ref = np.quantile(image_ref, [0.1, 0.25, 0.75, 0.9])
+    q_match = np.quantile(image_nm.get_voxel_grid(), [0.1, 0.25, 0.75, 0.9])
+
+    assert np.std(image_ref) - 0.01 < np.std(image_nm.get_voxel_grid()) < np.std(image_ref) + 0.01
+    for ii in np.arange(len(q_match)):
+        assert q_ref[ii] - 0.01 < q_match[ii] < q_ref[ii] + 0.01
+
+    # Test with normalisation
+    # No tissue mask.
+    data_no_mask = extract_features_and_images(
+        tissue_mask_type="none",
+        intensity_normalisation="match_reference_normalised",
+        intensity_normalisation_reference=image_ref,
+        **GENERIC_KWARGS
+    )
+
+    # Tissue mask
+    data_mask = extract_features_and_images(
+        tissue_mask_type="relative_range",
+        tissue_mask_range=[0.02, 1.00],
+        intensity_normalisation="match_reference_normalised",
+        intensity_normalisation_reference=image_ref,
+        **GENERIC_KWARGS
+    )
+
+    feature_data_nm = data_no_mask[0][0]
+    image_nm = data_no_mask[0][1][0]
+    mask_nm = data_no_mask[0][2][0]
+    feature_data_m = data_mask[0][0]
+    image_m = data_mask[0][1][0]
+    mask_m = data_mask[0][2][0]
+
+    assert len(feature_data_nm) == 1
+    assert len(feature_data_m) == 1
+    assert isinstance(mask_nm, BaseMask)
+    assert isinstance(mask_m, BaseMask)
+    assert isinstance(image_nm, MRImage)
+    assert isinstance(image_m, MRImage)
+
+    assert np.min(image_nm.get_voxel_grid()) == 0.0
+    assert np.max(image_nm.get_voxel_grid()) == 1.0
+    assert np.min(image_m.get_voxel_grid()) == 0.0
+    assert np.max(image_m.get_voxel_grid()) == 1.0
+    assert 0.009 < np.std(image_nm.get_voxel_grid()) < 0.010
+    assert 0.008 < np.std(image_m.get_voxel_grid()) < 0.009
