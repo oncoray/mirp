2e67bf20347f8475b9c057c71921a7514645c9ae
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2026-07-06 18:08:52 +0200

    Move details to the slice level.

--- mirp/_data_import/dicom_file_enhanced_pet.py
+++ mirp/_data_import/dicom_file_enhanced_pet.py
@@ -9,15 +9,26 @@ class ImageDicomFilePTMultiFrameStack(ImageDicomMultiFrameStack, ImageDicomFileP
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
+    @staticmethod
+    def _get_individual_frame_class():
+        return ImageDicomFilePTMultiFrameIndividual
+
+
+class ImageDicomFilePTMultiFrameIndividual(ImageDicomMultiFrameIndividual, ImageDicomFilePTMultiFrameStack):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.pet_unit: None | str = None
+        self.suv_unit: None | str = None
+
     def load_data(
             self,
             pet_suv_conversion: str = "body_weight",
             pet_autocorrect_administration_start: bool = True,
             **kwargs
     ):
-        # Handle PT multi-frame
-        image_data = super().load_data(**kwargs)
-
+        # Load data.
+        super().load_data(**kwargs)
+        
         if pet_suv_conversion != "none":
             # First we need to go the GML as unit.
             gml_factor = self._to_gml_conversion_factor(autocorrect_administration_start=pet_autocorrect_administration_start)
@@ -31,9 +42,45 @@ class ImageDicomFilePTMultiFrameStack(ImageDicomMultiFrameStack, ImageDicomFileP
         # Set image_data attribute.
         self.image_data = image_data
 
-    @staticmethod
-    def _get_individual_frame_class():
-        return ImageDicomFilePTMultiFrameIndividual
+    def _get_pet_unit(self):
+        # For enhanced PET, there is no PET units (0054,1001) attribute, and should either be extracted per-frame or
+        # from the shared group.
+
+        if self._get_n_frames() == 0:
+            return None
+
+        # Check the Real World Value Mapping Sequence(s) for available units.
+        real_world_units, _ = self._get_real_world_units()
+
+        # Check if there are any g/ml, cm2/ml or Bq/ml units.
+        pet_unit = [None] * self._get_n_frames()
+        for ii in range(len(pet_unit)):
+            if real_world_units is None or real_world_units[ii] is None:
+                continue
+            # g/ml
+            if real_world_units[ii] in []:
+                ...
+
+        # Try Rescale Type (0028,1054) from the Pixel Value Transformation Sequence (0028,9145) first. The DICOM
+        # standard specifies it as "US", but this is sometimes ignored.
+        pet_unit = self.get_pydicom_func_group_tag(
+            tag=(0x0028, 0x1054),
+            tag_type="str",
+            macro_dcm_seq=(0x0028, 0x9145),
+            default="US"
+        )
+
+        # Try Measurement Units Code Sequence (0040,08EA) in Real World Value Mapping Sequence (0040,9096).
+        real_world_value_mapping_sequence = self.image_metadata[(0x5200, 0x9229)][0]
+
+        pet_unit = get_pydicom_meta_tag(
+            dcm_seq=self.image_metadata,
+            tag=(0x0040, 0x08EA),
+            tag_type="float",
+            macro_dcm_seq=(0x0040, 0x9096)
+        )
+
+        return pet_unit
 
     def _to_gml_conversion_factor(self, autocorrect_administration_start=True) -> float:
         """To compute SUV, PET units need to be converted to BQML."""
@@ -55,49 +102,3 @@ class ImageDicomFilePTMultiFrameStack(ImageDicomMultiFrameStack, ImageDicomFileP
             )
 
         return conversion_factor
-
-    def _get_pet_unit(self):
-        # For enhanced PET, there is no PET units (0054,1001) attribute, and should either be extracted per-frame or
-        # from the shared group.
-
-        if self._get_n_frames() == 0:
-            return None
-
-        # Check the Real World Value Mapping Sequence(s) for available units.
-        real_world_units, _ = self._get_real_world_units()
-
-        # Check if there are any g/ml, cm2/ml or Bq/ml units.
-        pet_unit = [None] * self._get_n_frames()
-        for ii in range(len(pet_unit)):
-            if real_world_units is None or real_world_units[ii] is None:
-                continue
-            # g/ml
-            if real_world_units[ii] in []:
-                ...
-
-
-        # Try Rescale Type (0028,1054) from the Pixel Value Transformation Sequence (0028,9145) first. The DICOM
-        # standard specifies it as "US", but this is sometimes ignored.
-        pet_unit = self.get_pydicom_func_group_tag(
-            tag=(0x0028, 0x1054),
-            tag_type="str",
-            macro_dcm_seq=(0x0028,0x9145),
-            default="US"
-        )
-
-        # Try Measurement Units Code Sequence (0040,08EA) in Real World Value Mapping Sequence (0040,9096).
-        real_world_value_mapping_sequence = self.image_metadata[(0x5200, 0x9229)][0]
-
-        pet_unit = get_pydicom_meta_tag(
-            dcm_seq=self.image_metadata,
-            tag=(0x0040, 0x08EA),
-            tag_type="float",
-            macro_dcm_seq=(0x0040, 0x9096)
-        )
-
-        return pet_unit
-
-
-class ImageDicomFilePTMultiFrameIndividual(ImageDicomMultiFrameIndividual, ImageDicomFilePTMultiFrameStack):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
