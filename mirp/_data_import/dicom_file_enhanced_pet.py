fb3590faef380dc8766cb23434051c61695280d2
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2026-07-13 08:34:57 +0200

    Complete the _set_pet_unit method.

--- mirp/_data_import/dicom_file_enhanced_pet.py
+++ mirp/_data_import/dicom_file_enhanced_pet.py
@@ -1,4 +1,5 @@
 import copy
+import warnings
 
 from mirp._data_import.dicom_file_pet import ImageDicomFilePT
 from mirp._data_import.dicom_multi_frame import ImageDicomMultiFrameStack, ImageDicomMultiFrameIndividual
@@ -34,6 +35,7 @@ class ImageDicomFilePTMultiFrameIndividual(ImageDicomMultiFrameIndividual, Image
         # Set pet and suv unit.
         self._set_pet_suv_unit()
 
+        # Only update image contents if conversion to SUV is required.
         if pet_suv_conversion != "none":
             # First we need to go the GML as unit.
             gml_factor = self._to_gml_conversion_factor(autocorrect_administration_start=pet_autocorrect_administration_start)
@@ -62,116 +64,68 @@ class ImageDicomFilePTMultiFrameIndividual(ImageDicomMultiFrameIndividual, Image
         # Strip external and internal whitespace and convert to lower case to avoid any case-sensitivity.
         value_unit = "".join(value_unit.split()).lower()
 
-        "{counts}"
-        "cnts"
-
-        "{counts}/s"
-        "cps"
-
-        "{propcounts}"
-        "propcnts"
-
-        "{propcounts}/s"
-        "propcps"
-
-        "cm2"
-        "cm2"
-
-        "%"
-        "pcnt"
-
-        "bq/ml"
-        "bqml"
-
-        "mg/min/ml"
-        "mgminml"
-
-        "umol/min/ml"
-        "umolminml"
-
-        "ml/min/g"
-        "mlming"
-
-        "ml/g"
-        "mlg"
-
-        "/cm"
-        "1cm"
-
-        "umol/ml"
-        "umolml"
-
-        "g/ml{suvbw}"
-        "bw"
-        "gml"
-
-        "g/ml{suvlbm}"
-        "lbm"
-
-        "g/ml{suvlbm(james128)}"
-        "lbmjames128"
-
-        "g/ml{suvlbm(janma)}"
-        "lbmjanma"
-
-        "cm2/ml{suvbsa}"
-        "bsa"
-        "cm2ml"
-        "cm2/ml"
-
-        "g/ml{suvibw}"
-        "ibw"
-
-        "none"
-        "mlminml"
-        "mlml"
-        "stddev"
-        
-
-        if value_unit in ["BQML", ]
-
-
-
-
-    def _get_pet_unit(self):
-        # For enhanced PET, there is no PET units (0054,1001) attribute, and should either be extracted per-frame or
-        # from the shared group.
-
-        if self._get_n_frames() == 0:
-            return None
-
-        # Check the Real World Value Mapping Sequence(s) for available units.
-        real_world_units, _ = self._get_real_world_units()
-
-        # Check if there are any g/ml, cm2/ml or Bq/ml units.
-        pet_unit = [None] * self._get_n_frames()
-        for ii in range(len(pet_unit)):
-            if real_world_units is None or real_world_units[ii] is None:
-                continue
-            # g/ml
-            if real_world_units[ii] in []:
-                ...
-
-        # Try Rescale Type (0028,1054) from the Pixel Value Transformation Sequence (0028,9145) first. The DICOM
-        # standard specifies it as "US", but this is sometimes ignored.
-        pet_unit = self.get_pydicom_func_group_tag(
-            tag=(0x0028, 0x1054),
-            tag_type="str",
-            macro_dcm_seq=(0x0028, 0x9145),
-            default="US"
-        )
-
-        # Try Measurement Units Code Sequence (0040,08EA) in Real World Value Mapping Sequence (0040,9096).
-        real_world_value_mapping_sequence = self.image_metadata[(0x5200, 0x9229)][0]
-
-        pet_unit = get_pydicom_meta_tag(
-            dcm_seq=self.image_metadata,
-            tag=(0x0040, 0x08EA),
-            tag_type="float",
-            macro_dcm_seq=(0x0040, 0x9096)
-        )
-
-        return pet_unit
+        if value_unit in ["bq/ml", "bqml"]:
+            self.pet_unit = "bq/ml"
+        elif value_unit in ["g/ml", "g/ml{suvbw}", "bw", "gml"]:
+            self.pet_unit = "g/ml"
+            self.suv_unit = "body_weight"
+            if value_unit in ["g/ml", "gml"]:
+                warnings.warn(
+                    f"The current intensity unit is g/ml, without further specification. Body-weight-corrected "
+                    f"standardised uptake values are assumed. {self.describe_self()}",
+                    UserWarning
+                )
+        elif value_unit in ["g/ml{suvlbm}", "lbm"]:
+            self.pet_unit = "g/ml"
+            self.suv_unit = "lean_body_mass_error"
+        elif value_unit in ["g/ml{suvlbm(james128)}", "lbmjames128"]:
+            self.pet_unit = "g/ml"
+            self.suv_unit = "lean_body_mass"
+        elif value_unit in ["g/ml{suvlbm(janma)}", "lbmjanma"]:
+            self.pet_unit = "g/ml"
+            self.suv_unit = "lean_body_mass_bmi"
+        elif value_unit in ["g/ml{suvibw}", "ibw"]:
+            self.pet_unit = "g/ml"
+            self.suv_unit = "ideal_body_weight"
+        elif value_unit in ["cm2/ml{suvbsa}", "bsa", "cm2ml", "cm2/ml"]:
+            self.pet_unit = "g/ml"
+            self.suv_unit = "body_surface_area"
+        elif value_unit in ["{counts}", "cnts"]:
+            self.pet_unit = "counts"
+        elif value_unit in ["{counts}/s", "cps"]:
+            self.pet_unit = "counts/s"
+        elif value_unit in ["{propcounts}", "propcnts"]:
+            self.pet_unit = "propcounts"
+        elif value_unit in ["{propcounts}/s", "propcps"]:
+            self.pet_unit = "propcounts/s"
+        elif value_unit in ["cm2"]:
+            self.pet_unit = "cm2"
+        elif value_unit in ["%", "pcnt"]:
+            self.pet_unit = "percent"
+        elif value_unit in ["mg/min/ml", "mgminml"]:
+            self.pet_unit = "mg/min/ml"
+        elif value_unit in ["umol/min/ml", "umolminml"]:
+            self.pet_unit = "µmol/min/ml"
+        elif value_unit in ["ml/min/g", "mlming"]:
+            self.pet_unit = "ml/min/g"
+        elif value_unit in ["ml/g", "mlg"]:
+            self.pet_unit = "ml/g"
+        elif value_unit in ["/cm", "1cm"]:
+            self.pet_unit = "1/cm"
+        elif value_unit in ["umol/ml", "umolml"]:
+            self.pet_unit = "µmol/ml"
+        elif value_unit in ["mlminml"]:
+            self.pet_unit = "ml/min/ml"
+        elif value_unit in ["mlml"]:
+            self.pet_unit = "ml/ml"
+        elif value_unit in ["stddev"]:
+            self.pet_unit = "stddev"
+        elif value_unit in ["none"]:
+            pass
+        else:
+            raise ValueError(
+                f"MIRP did not recognise the provided PET unit: {value_unit}. {self.describe_self()}"
+            )
 
     def _to_gml_conversion_factor(self, autocorrect_administration_start=True) -> float:
         """To compute SUV, PET units need to be converted to BQML."""
